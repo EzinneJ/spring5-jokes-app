fe69ba0deef8cd4788e22d7ce0059cc5037faca1
EzinneJ <chimejoyez@gmail.com>
2022-06-21 07:46:40 +0100

    added JokeController

---
+++ src/main/java/ezinne/springframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package ezinne.springframework.spring5jokesapp.controllers;
+
+import ezinne.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
