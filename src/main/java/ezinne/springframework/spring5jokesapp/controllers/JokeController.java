347dfffdee1c11513a43a6ee0839a655de64167a
EzinneJ <chimejoyez@gmail.com>
2022-06-21 08:17:14 +0100

    added html file as the view layer

--- src/main/java/ezinne/springframework/spring5jokesapp/controllers/JokeController.java
+++ src/main/java/ezinne/springframework/spring5jokesapp/controllers/JokeController.java
@@ -1,10 +1,12 @@
 package ezinne.springframework.spring5jokesapp.controllers;
 
 import ezinne.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JokeController {
     private final JokeService jokeService;
 
